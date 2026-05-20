b1cf436f78c9f5b5eb4cb1f09f2fe7f19e5acb36
Nikola Paunovic <nikola.paunovic.dm1@gmail.com>
2026-05-20 14:13:26 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Apartment from './pages/Apartment'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
+import Reservation from './pages/Reservation'
+
+function App () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apartments" element={<Apartment />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/reservation" element={<Reservation />} />
+      </Routes>
+    </BrowserRouter>
+  )
+  
+}
+export default App
